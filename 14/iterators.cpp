583a4a937e258195ed91483987a7796c581a97ed
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-03-01 20:12:20 +0000

    Finish lesson14 iterators

--- 14/iterators.cpp
+++ 14/iterators.cpp
@@ -17,5 +17,33 @@ int main(){
         std::cout << *itr << "\n";
     }
 
+    std::cout << "\n";
+
+    std::vector<int>::iterator itr2 = nums2.begin();
+    advance(itr2, 2);
+    std::cout << *itr2 << "\n";
+
+    std::cout << "\n";
+
+    auto itr3 = next(itr2, 1);
+    std::cout << *itr3 <<"\n";
+
+    std::cout << "\n";
+
+    auto itr4 = prev(itr2, 1);
+    std::cout << *itr4 << "\n";
+
+    std::cout << "\n";
+
+    std::vector<int> nums3 = {1,4,5,6};
+    std::vector<int> nums4 = {2,3};
+    auto itr5 = nums3.begin();
+    advance(itr5, 1);
+    copy(nums4.begin(), nums4.end(), inserter(nums3, itr5));
+    
+    for(int &i: nums3){
+        std::cout << i << "\n";
+    }
+
     return 0;
 }
