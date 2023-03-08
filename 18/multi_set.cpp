64f4edf64e2c9930a8829b7876795d5da50ed2e3
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-03-08 19:47:36 +0000

    Add lesson18 multi_set

--- 18/multi_set.cpp
+++ 18/multi_set.cpp
@@ -16,6 +16,7 @@ int main(){
     std::multiset<int> mset1 {1,1,2,3,4};
     mset1.insert(4);
     mset1.insert(0);
+
     if(!mset1.empty()){
         for(int i : mset1){
             std::cout << i << "\n";
